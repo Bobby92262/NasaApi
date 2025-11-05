7f6b52d28338915ea8bb2eb9d64bd8001217519a
Bobby92262 <bobbyfinn086@gmail.com>
2025-11-05 12:37:15 +0000

    Initial commit: NASA APOD API wrapper. - My API project.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./nasaApiOpenapi.json');
+const nasaUrl = `https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}`;
+
+app.get('/apod/today', async (req, res) => {
+    try{
+        const response = await axios.get(nasaUrl);
+        res.json(response.data)
+    } catch (error) {
+        res.status(500).json({error: 'NASA API is currently unavailable. Please try again later.' });
+    }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
